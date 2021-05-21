2e79bdd65b1b15f32e9be7e1a595bbf0b494edbd
Kiana Vang <56984381+kianavang@users.noreply.github.com>
2021-05-20 23:04:17 -0500

    Utility methods
    Utility methods for creating argv[] arrays and pausing execution

---
+++ util.c
@@ -0,0 +1,31 @@
+// util.c: provided functions to ease parsing and pausing.
+
+#include "commando.h"
+
+void parse_into_tokens(char input_command[], char *tokens[], int *ntok)
+// Parse the contents of input_command so that tokens[i] will point to
+// the ith space-separated string in it. Set ntok to the number of
+// tokens that are found.
+{
+  int i = 0;
+  char *tok = strtok(input_command," \n"); // Splits input_command str into tokens according to given delimiters.
+
+  while(tok!=NULL && i<ARG_MAX){ // ARG_MAX is 255
+    tokens[i] = tok;  // assign tokens to found string
+    i++;
+    tok = strtok(NULL," \n");
+  }
+  tokens[i] = NULL; // null terminate tokens to ease argv[] work
+  *ntok = i; // the number of tokens found
+  return;
+}
+
+// Sleep the running program for the given number of nanoseconds and
+// seconds.
+void pause_for(long nanos, int secs){
+  struct timespec tm = {
+    .tv_nsec = nanos,
+    .tv_sec  = secs,
+  };
+  nanosleep(&tm,NULL);
+}
